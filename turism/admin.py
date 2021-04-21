a7f907eee34091c50139cc8a7a461b0885bda279
Sebastian Herrera <sebastianhh1999@gmail.com>
2021-04-21 12:21:24 -0500

    Page

--- turism/admin.py
+++ turism/admin.py
@@ -1,3 +1,8 @@
+from turism.models import Place, TravelDate, Weather
 from django.contrib import admin
 
 # Register your models here.
+
+admin.register(Weather)
+admin.register(Place)
+admin.register(TravelDate)

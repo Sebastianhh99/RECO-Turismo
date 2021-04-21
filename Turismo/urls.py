33023da5e3f509d235ea3a647cc253453faac684
Sebastian Herrera <sebastianhh1999@gmail.com>
2021-04-21 12:59:08 -0500

    URL for static Files

--- Turismo/urls.py
+++ Turismo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,re_path
 from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
